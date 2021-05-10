79679a347b868a9ba94a8de2c4fc6e56c5585762
Laldhan-glitch <laldhantamang@gmail.com>
2021-05-10 12:04:48 +0545

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using EmployeeManagement.Entity;
+
+namespace EmployeeManagement.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<EmployeeManagement.Entity.Employees> Employees { get; set; }
+    }
+}
